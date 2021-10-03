fad188194cec514b11b2a5f76020f4a1a03f7831
MrSwagBoy07 <61626313+MrSwagBoy07@users.noreply.github.com>
2021-10-03 17:38:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var r = 0;
+var g = 50;
+var b=255;
+
+
+
+function setup() {
+  createCanvas(800,400);
+ 
+
+}
+function draw() {
+  ellipse(mouseX, mouseY, 20, 20);
+}
+function mousePressed() {
+  clear();
+  background(128);
+  background(r,g,b);  
+  r=map(mouseX,0,1200,0,255);
+  g=map(mouseX,50,900,0,255);
+  b=map(mouseX,100,600,0,255);
+  fill("white")
+  ellipse(mouseX,mouseY,10,10);
+
+ 
+
+
+  drawSprites();
+}
